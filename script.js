d5ebf03f3a90353be148e9261d3fd63cd495f8e5
darsh0820 <54519719+darsh0820@users.noreply.github.com>
2022-09-23 22:41:25 +0530

    Added scroll animation

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
 const loadText = document.querySelector('.loading-text')
-        const bg = document.querySelector('.bg')
-        const Bar = document.querySelector('.bar')
-        const loadBar = document.querySelector('.loading-bar')
+const bg = document.querySelector('.bg')
+const Bar = document.querySelector('.bar')
+const loadBar = document.querySelector('.loading-bar')
 
-        let load = 0
+let load = 0
 
-        let int = setInterval(blurring, 20)
+let int = setInterval(blurring, 20)
 
-        function blurring() {
-            load++
+function blurring() {
+    load++
 
-            if (load > 99) {
-                clearInterval(int)
-            }
+    if (load > 99) {
+        clearInterval(int)
+    }
 
-            //consle.log(load)
-            loadText.innerText = `Loading ${load}%`
-            loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    //consle.log(load)
+    loadText.innerText = `Loading ${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
 
-            Bar.style.opacity = scale(load, 0, 100, 1, 0)
-            loadBar.style.width = `${scale(load, 0, 100, 0, 400)}px`
+    Bar.style.opacity = scale(load, 0, 100, 1, 0)
+    loadBar.style.width = `${scale(load, 0, 100, 0, 200)}px`
 
-            bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale(number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)  => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
         }
-
-        https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-        function scale(number, inMin, inMax, outMin, outMax) {
-            return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+        else{
+            entry.target.classList.remove('show')
         }
+    })
+})
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el))
